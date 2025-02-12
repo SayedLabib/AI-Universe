0e7c05787b66169c71554e6c7ed6c8a6635a8375
Sayed Labib <98503095+SayedLabib@users.noreply.github.com>
2025-02-12 23:22:03 +0600

    updated

--- Js/app.js
+++ Js/app.js
@@ -1,3 +1,23 @@
 
-
 console.log('Connected');
+
+
+const loadData = async() =>
+    {
+        const res = await fetch('https://openapi.programming-hero.com/api/ai/tools');
+        const data = await res.json();
+
+        // console.log(data.data);
+        
+
+        data.data.tools.forEach((item) => 
+        {
+           
+             console.log(item);
+             
+        });
+            
+   
+    }; 
+
+ loadData();   
