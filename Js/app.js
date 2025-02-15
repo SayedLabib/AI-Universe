f1b97737647a2c37df6a63184935f9e39db11108
Sayed Labib <98503095+SayedLabib@users.noreply.github.com>
2025-02-15 19:17:42 +0600

    Display tools created

--- Js/app.js
+++ Js/app.js
@@ -8,82 +8,96 @@ const loadData = async() =>
         const data = await res.json();
 
         const tools = data.data?.tools;
+
+        displayTools(tools);
         
+    }   
+        
+    
+    const displayTools = (tool)=>{
+
+                    
+      const toolsContainer = document.getElementById('tools-container');
+
+      toolsContainer.innerHTML = '';
+
+
+    
+      tool.forEach((item) => 
+      {
+         
+           console.log(item);
+
+          const div = document.createElement('div');
           
-        const toolsContainer = document.getElementById('tools-container');
+          
 
-        toolsContainer.innerHTML = '';
-
-
-
-
-        tools.forEach((item) => 
-        {
+          div.innerHTML =
+          `
+          <div class="card bg-base-100 w-[487px] shadow-xl">
+        
+                  <img
+                  src="${item?.image}"
+                  alt="loading"
+                  class="rounded-xl mx-5" />
+        
+                  <div class="card-body h-auto">
+          
+                  <h2 class="card-title font-sans font-bold text-xl">Features</h2>
            
-             console.log(item);
+                  <div class="mt-4 font-sans font-normal text-[14px] text-[#585858]">
+                      <ol class="list-decimal list-inside">
+                          <li>${item?.features[0]}</li>
+                          <li>${item?.features[1]}</li>
+                          <li>${item?.features[2]}</li>
+                      </ol>
+                  </div>
 
-            const div = document.createElement('div');
-            
-            
-
-            div.innerHTML =
-            `
-            <div class="card bg-base-100 w-[487px] shadow-xl">
+          <hr class="mt-4">
           
-                    <img
-                    src="${item?.image}"
-                    alt="loading"
-                    class="rounded-xl mx-5" />
           
-                    <div class="card-body h-auto">
-            
-                    <h2 class="card-title font-sans font-bold text-xl">Features</h2>
-             
-                    <div class="mt-4 font-sans font-normal text-[14px] text-[#585858]">
-                        <ol class="list-decimal list-inside">
-                            <li>${item?.features[0]}</li>
-                            <li>${item?.features[1]}</li>
-                            <li>${item?.features[2]}</li>
-                        </ol>
-                    </div>
+          <div class=" flex items-center justify-between mt-4">
+              
+             <div class="">
 
-            <hr class="mt-4">
-            
-            
-            <div class=" flex items-center justify-between mt-4">
-                
-               <div class="">
+                <h3 class="font-sans font-semibold text-xl">${item?.name}</h3>
+                       
+                 <div class="flex justify-around mt-5 gap-2">
+                   <img src="images/Calendar.png" alt="">
+                   <p>${item?.published_in}</p>
+      
+                 </div>
 
-                  <h3 class="font-sans font-semibold text-xl">${item?.name}</h3>
-                         
-                   <div class="flex justify-around mt-5 gap-2">
-                     <img src="images/Calendar.png" alt="">
-                     <p>${item?.published_in}</p>
-        
-                   </div>
+             </div>        
 
-               </div>        
-
-              <div class="card-actions ">
-                <button class="btn btn-circle border-none shadow-none bg-[#FEF7F7]">
-                  <img src="images/arrow.png" alt="">
-                </button>
-              </div>   
-
-            </div>
-            
+            <div class="card-actions ">
+              <button class="btn btn-circle border-none shadow-none bg-[#FEF7F7]">
+                <img src="images/arrow.png" alt="">
+              </button>
+            </div>   
 
           </div>
+          
+
         </div>
-                    
-            
-            `
-        
-            toolsContainer.appendChild(div); 
-        });
+      </div>
+                  
+          
+          `
+      
+          toolsContainer.appendChild(div); 
+      });
+ 
+
+
+
+
+
+
+    };
+
         
    
-    }; 
 
 
 
